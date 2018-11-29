a5ff111a03b3615494b0aaf7033f20bbfe79ee30
jchanme <39323414+jchanme@users.noreply.github.com>
2018-11-29 15:27:59 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Forum Customization",
+    "version": "1.0",
+    "depends": ['website_forum',
+                ],
+    'author': 'Safyric Co., Ltd.',
+    'license': 'AGPL-3',
+    'installable': True,
+}
